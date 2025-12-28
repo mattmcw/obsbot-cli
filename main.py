baa73d29c23599abf9b79b8a09a0755d924fa8e6
mattmcw <hi@mmcwilliams.com>
2025-12-28 13:04:13 -0500

    Implement some validation on command line arguments

--- main.py
+++ main.py
@@ -2,84 +2,20 @@ from argparse import ArgumentParser
 from dotenv import dotenv_values
 from pythonosc import udp_client
 
-"""
-/OBSBOT/WebCam/General/Connected
-/OBSBOT/WebCam/General/ConnectedResp
-/OBSBOT/WebCam/General/Disconnected
-/OBSBOT/WebCam/General/SelectDevice
-* /OBSBOT/WebCam/General/WakeSleep
-# /OBSBOT/WebCam/General/ResetGimbal
-/OBSBOT/WebCam/General/SetZoom
-/OBSBOT/WebCam/General/SetZoomSpeed
-/OBSBOT/WebCam/General/SetZoomMax
-/OBSBOT/WebCam/General/SetZoomMin
-/OBSBOT/WebCam/General/SetGimbalUp
-/OBSBOT/WebCam/General/SetGimbalDown
-/OBSBOT/WebCam/General/SetGimbalLeft
-/OBSBOT/WebCam/General/SetGimbalRight
-/OBSBOT/WebCam/General/SetView
-/OBSBOT/WebCam/General/SetGimMotorDegree
-/OBSBOT/WebCam/General/SetGimMotorDegreeEx
-/OBSBOT/WebCam/General/SetMirror
-/OBSBOT/WebCam/General/SetPCRecording
-/OBSBOT/WebCam/General/PCSnapshot
-
-/OBSBOT/WebCam/General/SetAutoFocus
-/OBSBOT/WebCam/General/SetManualFocus
-
-/OBSBOT/WebCam/General/SetAutoExposure
-/OBSBOT/WebCam/General/SetExposureCompensate
-/OBSBOT/WebCam/General/SetShutterSpeed
-/OBSBOT/WebCam/General/SetISO
-/OBSBOT/WebCam/General/SetAutoWhiteBalance
-/OBSBOT/WebCam/General/SetColorTemperature
-
-/OBSBOT/WebCam/General/GetDeviceInfo
-/OBSBOT/WebCam/General/DeviceInfo
-/OBSBOT/WebCam/General/GetZoomInfo
-/OBSBOT/WebCam/General/ZoomInfo
-/OBSBOT/WebCam/General/GetGimbalPosInfo
-/OBSBOT/WebCam/General/GetGimbalPosInfoResp
-
-/OBSBOT/Camera/Tail/SetFocusMode
-/OBSBOT/Camera/Tail/SetAiMode
-/OBSBOT/Camera/Tail/SetTrackingSpeed
-/OBSBOT/Camera/Tail/SetPanTrackingSpeed
-/OBSBOT/Camera/Tail/SetTiltTrackingSpeed
-/OBSBOT/Camera/Tail/SetPanAxisLock
-/OBSBOT/Camera/Tail/SetTiltAxisLock
-/OBSBOT/Camera/Tail/SetRecording
-/OBSBOT/Camera/Tail/Snapshot
-/OBSBOT/Camera/Tail/TriggerPreset
-
-/OBSBOT/WebCam/Tiny/ToggleAILock
-/OBSBOT/WebCam/Tiny/TriggerPreset
-/OBSBOT/WebCam/Tiny/SetAiMode
-/OBSBOT/WebCam/Tiny/SetTrackingMode
-/OBSBOT/WebCam/Tiny/GetAiTrackingInfo
-/OBSBOT/WebCam/Tiny/AiTrackingInfo
-/OBSBOT/WebCam/Tiny/GetPresetPositionInfo
-/OBSBOT/WebCam/Tiny/PresetPositionInfo
-
-/OBSBOT/WebCam/Meet/SetVirtualBackground
-/OBSBOT/WebCam/Meet/SetAutoFraming
-/OBSBOT/WebCam/Meet/SetStandardMode
-/OBSBOT/WebCam/Meet/GetVirtualBackgroundInfo
-/OBSBOT/WebCam/Meet/VirtualBackgroundInfo
-/OBSBOT/WebCam/Meet/GetAutoFramingInfo
-/OBSBOT/WebCam/Meet/AutoFramingInfo
-"""
-
 CAMERAS = ['General', 'Tiny', 'Tail', 'Meet']
 CONFIG_VALUES = ['host', 'port', 'camera']
+
 COMMANDS = {
 	"WakeSleep" : {
 		"address" : "/OBSBOT/WebCam/General/WakeSleep",
-		"value" : 1
+		"value" : 1,
+		"options" : [0, 1],
+		"type" : "simple"
 	},
 	"ResetGimbal" : {
 		"address" : "/OBSBOT/WebCam/General/ResetGimbal",
-		"value" : 1
+		"value" : 0,
+		"type" : "simple"
 	}
 }
 
@@ -106,7 +42,7 @@ def list () :
 	for key in keys :
 		print(f"{key}\t{COMMANDS[key]['address']}")
 
-def send_osc_command (address, value):
+def send_osc_command_simple(address, value):
 	"""Sends an OSC message with a single integer argument."""
 	try:
 		client = udp_client.SimpleUDPClient(Config["host"], Config["port"])
@@ -124,9 +60,25 @@ def command (args, parser) :
 	if args.command in COMMANDS :
 		cmd = COMMANDS[args.command]["address"]
 		val = COMMANDS[args.command]["value"]
-		if getattr(args, 'value') is not None :
-			val = getattr(args, 'value')
-		send_osc_command(cmd, val)
+
+		if getattr(args, "value") is not None :
+			if "options" in COMMANDS[args.command]:
+				if int(args.value) not in COMMANDS[args.command]["options"] :
+					print(f"Value {int(args.value)} not a valid value for command {cmd}")
+					exit(3)
+				else :
+					val = int(args.value)
+			elif "range" in COMMANDS[args.command]: 
+				if args.value < COMMANDS[args.command]["range"][0] or args.value > COMMANDS[args.command]["range"][1] :
+					print(f"Value {args.value} is not in range {COMMANDS[args.command]['range'][0]}->{COMMANDS[args.command]['range'][1]} for command {args.command}")
+					exit(4)
+				else : 
+					val = args.value
+
+		if COMMANDS[args.command]["type"] == "simple" :
+			send_osc_command_simple(cmd, val)
+		# getter
+		# 
 	else :
 		print(f"Command {args.command} is not valid")
 		parser.print_help()
@@ -140,17 +92,17 @@ def main () :
 	parser.add_argument("-c", "--camera", choices=CAMERAS, help=f"OBSBOT camera type to send commands for [General/Tiny/Meet/Tail] (default: {Config['camera']})")
 	parser.add_argument("-l", "--list", action="store_true", help="List all available commands for camera type")
 	parser.add_argument("command", type=str, nargs="?", help="Command to issue to OBSBOT camera over OSC")
-	parser.add_argument("value", type=str, nargs="?", help="Optional value to send with command")
+	parser.add_argument("value", type=float, nargs="?", help="Optional value to send with command")
 	args = parser.parse_args()
 	
 	env = dotenv_values(args.env)
 
 	update_config(args, env)
+
 	if args.list :
 		list()
 	else :
 		command(args, parser)
-
 
 if __name__ == "__main__":
 	main()
